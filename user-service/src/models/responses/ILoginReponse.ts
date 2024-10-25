5c850bbec2194b087c29eca9c25551a7883ce92b
M4JD <31166490+M4JD@users.noreply.github.com>
2024-10-25 19:23:37 +0300

    add base response to auth controller and add responses types

---
+++ user-service/src/models/responses/ILoginReponse.ts
@@ -0,0 +1,4 @@
+export default interface ILoginResponse {
+  token: string;
+  id: string;
+}
---
+++ user-service/src/models/responses/IRegisterResponse.ts
@@ -0,0 +1,5 @@
+export default interface IRegisterResponse {
+  id: string;
+  name: string;
+  email: string;
+}
