dd672dd4a3cad9e62f957a3702b69c802842ae51
M4JD <31166490+M4JD@users.noreply.github.com>
2024-10-25 19:45:24 +0300

    update auth controller and add requests models to API

--- user-service/src/controllers/AuthController.ts
+++ user-service/src/controllers/AuthController.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { User } from "../database";
 import { ApiError, encryptPassword, isPasswordMatch } from "../utils";
@@ -7,6 +7,9 @@ import { IUser } from "../database";
 import IRegisterResponse from "../models/responses/IRegisterResponse";
 import { IBaseResponse } from "../models/IBaseResponse";
 import ILoginResponse from "../models/responses/ILoginReponse";
+import ILoginRequest from "../models/requests/ILoginRequest";
+import IForgetPasswordRequest from "../models/requests/IForgetPasswordRequest";
+import IRegisterRequest from "../models/requests/IRegisterRequest";
 
 const jwtSecret = config.JWT_SECRET as string;
 const COOKIE_EXPIRATION_DAYS = 90; // cookie expiration in days
@@ -23,16 +26,16 @@ const register = async (req: any, res: any) => {
   try {
     req = req as Request;
     res = res as Response;
-    const { name, email, password } = req.body;
-    const userExists = await User.findOne({ email });
+    const request = req.body as IRegisterRequest;
+    const userExists = await User.findOne({ email: request.email });
     if (userExists) {
       throw new ApiError(400, "User already exists!");
     }
 
     const user = await User.create({
-      name,
-      email,
-      password: await encryptPassword(password),
+      name: request.name,
+      email: request.email,
+      password: await encryptPassword(request.password),
     });
 
     const Response: IBaseResponse<IRegisterResponse> = {
@@ -68,10 +71,40 @@ const login = async (req: any, res: any) => {
   try {
     req = req as Request;
     res = res as Response;
-    const { email, password } = req.body;
-    const user = await User.findOne({ email }).select("+password");
-    if (!user || !(await isPasswordMatch(password, user.password as string))) {
+    const request = req.body as ILoginRequest;
+    const user = await User.findOne({ email: request.email }).select(
+      "+password"
+    );
+    if (!user || !(await isPasswordMatch(request.password, user.password))) {
       throw new ApiError(400, "Incorrect email or password");
+    }
+    const token = await createSendToken(user!, res);
+    const Response: IBaseResponse<ILoginResponse> = {
+      data: {
+        id: user.id,
+        token,
+      },
+      message: "User logged in successfully!",
+      status: 200,
+    };
+
+    return res.json(Response);
+  } catch (error: any) {
+    return res.json({
+      status: 500,
+      message: error.message,
+    });
+  }
+};
+
+const forgetPassword = async (req: any, res: any) => {
+  try {
+    req = req as Request;
+    res = res as Response;
+    const request = req.body as IForgetPasswordRequest;
+    const user = await User.findOne({ email: request.email });
+    if (!user) {
+      throw new ApiError(400, "Incorrect email");
     }
     const token = await createSendToken(user!, res);
     const Response: IBaseResponse<ILoginResponse> = {
