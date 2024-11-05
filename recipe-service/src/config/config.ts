843fcd680987fdb2e0c944f39086c4612c8115f3
M4JD <31166490+M4JD@users.noreply.github.com>
2024-11-05 19:08:04 +0200

    init recipe service

---
+++ recipe-service/src/config/config.ts
@@ -0,0 +1,36 @@
+import { config } from "dotenv";
+
+const configFile = `./.env`;
+config({ path: configFile });
+
+const {
+  MONGO_URI,
+  PORT,
+  JWT_SECRET,
+  NODE_ENV,
+  MESSAGE_BROKER_URL,
+  FRONT_END_URL,
+  EMAIL_PASSWORD,
+  EMAIL,
+  BACK_END_LINK,
+  YOUR_COMPANY_LOGO_URL,
+  APPNAME
+} = process.env;
+const corsOptions = {
+  origin: FRONT_END_URL,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+export default {
+  FRONT_END_URL,
+  YOUR_COMPANY_LOGO_URL,
+  BACK_END_LINK,
+  APPNAME,
+  EMAIL_PASSWORD,
+  EMAIL,
+  MONGO_URI,
+  PORT,
+  JWT_SECRET,
+  env: NODE_ENV,
+  msgBrokerURL: MESSAGE_BROKER_URL,
+  corsOptions,
+};
